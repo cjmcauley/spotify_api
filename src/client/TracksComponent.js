9c18d42eb95f51a3917c618c316ce840262a92e3
cjmcauley <christopher.mcauley@gmail.com>
2019-11-12 08:21:54 +0000

    added track name

--- src/client/TracksComponent.js
+++ src/client/TracksComponent.js
@@ -3,7 +3,10 @@ import React from 'react';
 const TracksComponent = (props) => {
     const activityNodes = props.topTracks.map((item, index)=> {
         return (
-          <h2 key={index}>{item.artists[0].name}</h2>
+            <div key={index}>
+          <h2 >{item.artists[0].name}</h2>
+          <h3>{item.name}</h3>
+          </div>
         )
       });
       return(
