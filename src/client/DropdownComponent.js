f5f089ddf01cb79a5fac3cf6bb51c7c1045a0e48
cjmcauley <christopher.mcauley@gmail.com>
2019-12-04 14:00:15 +0000

    slider removed

--- src/client/DropdownComponent.js
+++ src/client/DropdownComponent.js
@@ -1,42 +1,30 @@
-import React, { Component } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Slider from '@material-ui/core/Slider';
-import Typography from '@material-ui/core/Typography';
-import './styles.css'
+import React, { Component } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Slider from "@material-ui/core/Slider";
+import Typography from "@material-ui/core/Typography";
+import "./styles.css";
 
 class DropdownComponent extends Component {
   constructor(props) {
     super(props);
 
-    this.handleChange = this.handleChange.bind(this)
-
+    this.handleChange = this.handleChange.bind(this);
   }
 
   handleChange = (index, value) => {
-    this.props.handleChangeTracks(index, value)
-    this.props.changeTopTracks()
-  }
+    this.props.handleChangeTracks(index, value);
+    this.props.changeTopTracks();
+  };
 
   render() {
-  return(
-        <div  >
-      <Typography id="discrete-slider" gutterBottom>
-        Select How Many Tracks to See
-      </Typography>
-      <Slider
-        defaultValue={5}
-        aria-labelledby="discrete-slider"
-        step={5}
-        marks
-        min={5}
-        max={50}
-        valueLabelDisplay="on"
-        onChange={this.handleChange}
-      />
-        </div>
-      )
-    }
+    return (
+      <div>
+        <Typography id="discrete-slider" gutterBottom>
+          Select How Many Tracks to See
+        </Typography>
+      </div>
+    );
   }
-  
+}
 
 export default DropdownComponent;
