8162b22e2de8ac0bb1eeb653e5883f87e1d66de6
cjmcauley <christopher.mcauley@gmail.com>
2019-12-07 16:49:28 +0000

    added second dropdown

--- src/client/DropdownComponent.js
+++ src/client/DropdownComponent.js
@@ -3,7 +3,12 @@ import Dropdown from "react-dropdown";
 import Typography from "@material-ui/core/Typography";
 import "./styles.css";
 
-const options = ["5", "10", "15", "20", "25", "30"];
+const days = ["5", "10", "15", "20", "25", "30"];
+const period = [
+  { tag: "last 4 weeks", value: "short_term" },
+  { tag: "last 6 months", value: "medium_term" },
+  { tag: "all time", value: "long_term" }
+];
 
 class DropdownComponent extends Component {
   constructor(props) {
@@ -25,15 +30,26 @@ class DropdownComponent extends Component {
     const defaultOption = this.state.selected;
 
     return (
-      <div>
-        <Typography>Select How Many Tracks to See</Typography>
-        <Dropdown
-          options={options}
-          onChange={this._onSelect}
-          value={defaultOption}
-          placeholder="5"
-        />
-      </div>
+      <>
+        <div>
+          <Typography>Select How Many Tracks to See</Typography>
+          <Dropdown
+            options={days}
+            onChange={this._onSelect}
+            value={defaultOption}
+            placeholder="5"
+          />
+        </div>
+        <div>
+          <Typography>Select Time Scale</Typography>
+          <Dropdown
+            options={period}
+            onChange={this._onSelect}
+            value={defaultOption}
+            placeholder="5"
+          />
+        </div>
+      </>
     );
   }
 }
